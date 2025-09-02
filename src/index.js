d04c07933f87318b8e8fb8733a2a59dbb4007d2d
merkusvictory <mvhilario23@gmail.com>
2025-09-01 21:03:47 -0400

    minor typo fix

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ client.on('ready', (c) => {
 
         const keys = Object.keys(jsonStats2)
         for (let i = 0; i < keys.length; i++) {
-            const memberId = keys[i]
+            const memberId = keys[i];
             if (jsonStats2[memberId]["isBounty"].active && jsonStats2[memberId]["isBounty"].alive) {
                 // if bounty survived the day
                 channel.send(`😎 Bounty **${jsonStats2[memberId]["name"]}** survived the day! 😎`);
