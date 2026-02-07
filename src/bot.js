c022a0f3facad7a4b935c859586bafc6ff309a74
merkusvictory <mvhilario23@gmail.com>
2026-02-07 16:29:35 -0500

    accumulation message adjustment

--- src/bot.js
+++ src/bot.js
@@ -192,7 +192,7 @@ client.on('messageCreate', async (message) => {
             let reply_message = `🔫 **${message.member.displayName}** just sniped${mentioned_members_output}! 🔫`;
             // sending marked transfer message
             if (senderData.isMarked) {
-                reply_message += `\n🩸 Accumulation raised to **${senderData["accumulation"] + 1}**! 🩸`;
+                reply_message += `\n🩸 Accumulation raised to **${senderData["accumulation"] + mentioned_members.length}**! 🩸`;
             }
             if (isMarkedTransfer) {
                 reply_message += `\n🩸 **${message.member.displayName}** sniped marked **<@${stolenMark}>** for ${stolenMarkPoints} accumulation points! 🩸`;
